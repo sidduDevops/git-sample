bb75e2fbeff2962deefb74225d416bf128f7daf9
Sindhu-Windows <sindhudhoni@gmail.com>
2024-09-24 13:01:32 +0530

    Saving modified 3rdd version install.sh

--- 07-install.sh
+++ 07-install.sh
@@ -6,9 +6,24 @@ if [ $ID -ne 0 ]
     then 
 
     echo "ERROR:: Please run this script with root access"
+    exit 1
 
 else
 
     echo "You are a root user"
 
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+    
+    then
+
+    echo "Error::Installing MYSQL is FAILED"
+    exit 1
+
+    else
+
+    echo "Instaalling MYSQL is success"
 fi
