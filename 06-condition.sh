7f8bd2025a299e2373e90e4e68ba725da8a3a4f4
Sindhu-Windows <sindhudhoni@gmail.com>
2024-09-24 12:40:15 +0530

    Saving modified condition.sh

--- 06-condition.sh
+++ 06-condition.sh
@@ -4,8 +4,12 @@ NUMBER=100
 
 if[$NUMBER -gt 50]
 
-echo "Given number is greater than 50"
+then
+
+    echo "Given number is greater than 50"
 
 else
 
-echo "Not greater"
+    echo "Not greater"
+
+if
